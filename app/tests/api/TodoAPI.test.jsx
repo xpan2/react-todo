41f500384c06c4b8421398124fe1636d790665d0
xpan2 <xpan@walmartlabs.com>
2016-10-28 14:10:04 -0700

    Added TodoAPI and tests

---
+++ app/tests/api/TodoAPI.test.jsx
@@ -0,0 +1,56 @@
+var expect = require('expect');
+
+var TodoAPI = require('TodoAPI');
+
+describe('TodoAPI', () => {
+  beforeEach(() => {
+    localStorage.removeItem('todos');
+  });
+  it('should exist', () => {
+    expect(TodoAPI).toExist();
+  });
+
+  describe('setTodos', () => {
+    it('should set valid todos array', () => {
+      var todos = [{
+        id: 12,
+        text: 'todo1',
+        completed: false
+      }];
+      TodoAPI.setTodos(todos);
+
+      var actualTodos = JSON.parse(localStorage.getItem('todos'));
+      expect(actualTodos).toEqual(todos);
+    });
+
+    it('should not set if invalid input', () => {
+      var todos = {
+        id: 12,
+        text: 'todo1',
+        completed: false
+      };
+      TodoAPI.setTodos(todos);
+
+      var actualTodos = JSON.parse(localStorage.getItem('todos'));
+      expect(actualTodos).toBe(null);
+    });
+  });
+
+  describe('getTodos', () => {
+    it('should return empty array for bad localStorage data', () => {
+      var actualTodos = TodoAPI.getTodos();
+      expect(actualTodos).toEqual([]);
+    });
+
+    it('should return array for valid data', () => {
+      var todos = [{
+        id: 12,
+        text: 'todo1',
+        completed: false
+      }];
+      localStorage.setItem('todos', JSON.stringify(todos));
+      var actualTodos = TodoAPI.getTodos();
+      expect(actualTodos).toEqual(todos);
+    });
+  });
+});
